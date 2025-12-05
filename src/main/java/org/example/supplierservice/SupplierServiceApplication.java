928d0987f30d7cad5940d3c3b0915d785f025861
murpalan <muralidhar.palaniappan@capgemini.com>
2025-12-05 13:28:18 +0100

    Testing CI CD

--- src/main/java/org/example/supplierservice/SupplierServiceApplication.java
+++ src/main/java/org/example/supplierservice/SupplierServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SupplierServiceApplication {
     public static void main(String[] args) {
+        System.out.println("Welcome");
         SpringApplication.run(SupplierServiceApplication.class, args);
     }
 }
